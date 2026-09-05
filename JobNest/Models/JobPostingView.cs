ab77e19f3a9548cc04c7b13a9d509b4668e70e3e
MAK <muxd.aslam@gmail.com>
2026-09-05 17:21:37 +0530

    feat(models): add JobPostingView view model

---
+++ JobNest/Models/JobPostingView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobNest.Models
+{
+    public class JobPostingView
+    {
+        public int JobId { get; set; }
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string JobTitle { get; set; }
+        public int ExperienceRequired { get; set; }
+        public string RequiredSkills { get; set; }
+        public string JobLocation { get; set; }
+        public string RequiredQualification { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime PostDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string JobStatus { get; set; }
+    }
+}
