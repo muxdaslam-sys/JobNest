dee54c017e580ba11eefff19c77a3ba1b572b715
MAK <muxd.aslam@gmail.com>
2026-09-06 22:30:47 +0530

    feat(models): add JobApplicationView model to resolve EDM ambiguity

--- JobNest/Models/JobPostingView.cs
+++ JobNest/Models/JobPostingView.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -19,5 +20,29 @@ namespace JobNest.Models
         public DateTime PostDate { get; set; }
         public DateTime EndDate { get; set; }
         public string JobStatus { get; set; }
+    }
+
+    public class JobApplicationView
+    {
+        public int JobApplicationId { get; set; }
+        public int EmployeeId { get; set; }
+        public int JobId { get; set; }
+
+        // Resume file for upload
+        public HttpPostedFileBase ResumeFile { get; set; }
+        public string Resume { get; set; }
+
+        // Job details for display
+        public string JobTitle { get; set; }
+        public string CompanyName { get; set; }
+        public string JobLocation { get; set; }
+        public decimal Salary { get; set; }
+        public int ExperienceRequired { get; set; }
+
+        // Application status
+        public bool AlreadyApplied { get; set; }
+        public DateTime? ApplicationDate { get; set; }
+        public string ApplicationStatus { get; set; }
+        public string ResumePath { get; set; }
     }
 }
